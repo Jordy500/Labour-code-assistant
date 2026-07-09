80ef57a7fd962f6550ad7a733991f5742f6bffa2
jordy500 <jordanngueko@gmail.com>
2026-07-09 14:16:01 +0200

    chore: save work before merge

---
+++ src/config 2.py
@@ -0,0 +1,9 @@
+CODE_TRAVAIL_ID = "LEGITEXT000006072050"
+
+OAUTH_TOKEN_URL = "https://sandbox-oauth.piste.gouv.fr/api/oauth/token"
+API_BASE_URL = "https://sandbox-api.piste.gouv.fr/dila/legifrance/lf-engine-app"
+
+EMBEDDING_MODEL_NAME = "paraphrase-multilingual-mpnet-base-v2"
+GROQ_MODEL_NAME = "llama-3.3-70b-versatile"
+
+
